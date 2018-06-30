1dc5234af66696760cdee3cc59596a804c05453b
Ivan <ivan.trashchenko@gmail.com>
2018-06-30 12:00:01 +0300

    Fix Task 1

---
+++ ArrayIntegerExtensions.Tests/ArrayIntegerExtensionTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayIntegerExtensions.Tests
+{
+    [TestClass]
+    public class ArrayIntegerExtensionTests
+    {
+        [TestMethod]
+        public void MergeSort_WithPositiveNumbers_Success()
+        {
+            int[] array = { 3, 26, 5, 12, 1 };
+            int[] expected = { 1, 3, 5, 12, 26 };
+
+            ArrayIntegerExtension.MergeSort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void MergeSort_WithPositiveAndNegativeNumbers_Success()
+        {
+            int[] array = { 34, -5, 12, 56, 1, 0, 7, 7, -7 };
+            int[] expected = { -7, -5, 0, 1, 7, 7, 12, 34, 56 };
+
+            ArrayIntegerExtension.MergeSort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void MergeSort_WithNullArgument_ThrowArgumentNullException()
+        {
+            int[] array = null;
+
+            ArrayIntegerExtension.MergeSort(array);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void MergeSort_EmptyArray_ThrowArgumentException()
+        {
+            int[] array = { };
+
+            ArrayIntegerExtension.MergeSort(array);
+        }
+
+        [TestMethod]
+        public void MergeSort_WithOneElement_Success()
+        {
+            int[] array = { 1 };
+            int[] expected = { 1 };
+
+            ArrayIntegerExtension.MergeSort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void QuickSort_WithPositiveNumbers_Success()
+        {
+            int[] array = { 3, 26, 5, 12, 1 };
+            int[] expected = { 1, 3, 5, 12, 26 };
+
+            ArrayIntegerExtension.QuickSort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void QuickSort_WithPositiveAndNegativeNumbers_Success()
+        {
+            int[] array = { 34, -5, 12, 56, 1, 0, 7, 7, -7 };
+            int[] expected = { -7, -5, 0, 1, 7, 7, 12, 34, 56 };
+
+            ArrayIntegerExtension.QuickSort(array);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void QuickSort_WithNullArgument_ThrowArgumentNullException()
+        {
+            int[] array = null;
+
+            ArrayIntegerExtension.QuickSort(array, 0, 2);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void QuickSort_EmptyArray_ThrowArgumentException()
+        {
+            int[] array = { };
+
+            ArrayIntegerExtension.QuickSort(array, 0, 2);
+        }
+
+        [TestMethod]
+        public void QuickSort_WithStartGreaterThanOrEqualsEnd_Success()
+        {
+            int[] array = { 34, -5, 12, 56, 1, 0, 7, 7, -7 };
+            int[] expected = { 34, -5, 12, 56, 1, 0, 7, 7, -7 };
+
+            ArrayIntegerExtension.QuickSort(array, 3, 2);
+
+            CollectionAssert.AreEqual(expected, array);
+        }
+    }
+}
---
+++ ArrayIntegerExtensions/ArrayIntegerExtension.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace ArrayIntegerExtensions
+{
+    /// <summary>
+    /// Class of sort methods.
+    /// </summary>
+    public static class ArrayIntegerExtension
+    {
+        #region Sorting Methods
+        /// <summary>
+        /// Method which performs merge sort for an array.
+        /// </summary>
+        /// <param name="array">Array to be sorted.</param>
+        public static void MergeSort(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            MergeSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Method which performs merge sort for an array with specific left border and specific right border.
+        /// </summary>
+        /// <param name="array">Array to be sorted.</param>
+        /// <param name="start">Left border of array.</param>
+        /// <param name="end">Right border of array.</param>
+        public static void MergeSort(int[] array, int start, int end)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            if (start < end)
+            {
+                int middle = (start / 2) + (end / 2);
+                MergeSort(array, start, middle);
+                MergeSort(array, middle + 1, end);
+                Merge(array, start, middle, end);
+            }
+        }
+
+        /// <summary>
+        /// Method which performs quick sort for an array.
+        /// </summary>
+        /// <param name="array">Array to be sorted.</param>
+        public static void QuickSort(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Method which performs quick sort for an array with specific left border and specific right border.
+        /// </summary>
+        /// <param name="array">Array to be sorted.</param>
+        /// <param name="start">Left border of array.</param>
+        /// <param name="end">Right border of array.</param>
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException(nameof(array));
+            }
+
+            int left = start, right = end;
+            int pivot = array[(left + right) / 2];
+
+            while (left <= right)
+            {
+                while (array[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (array[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left <= right)
+                {
+                    Swap(ref array[left++], ref array[right--]);
+                }
+            }
+
+            if (start < right)
+            {
+                QuickSort(array, start, right);
+            }
+
+            if (end > left)
+            {
+                QuickSort(array, left, end);
+            }
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Method which merges elements into array.
+        /// </summary>
+        /// <param name="array">Original array.</param>
+        /// <param name="start">Left border.</param>
+        /// <param name="middle">Index of middle element.</param>
+        /// <param name="end">Right border.</param>
+        private static void Merge(int[] array, int start, int middle, int end)
+        {
+            int left = start;
+            int right = middle + 1;
+            int[] tempArray = new int[(end - start) + 1];
+            int marker = 0;
+
+            while ((left <= middle) && (right <= end))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[marker++] = array[left++];
+                }
+                else
+                {
+                    tempArray[marker++] = array[right++];
+                }
+            }
+
+            if (left <= middle)
+            {
+                while (left <= middle)
+                {
+                    tempArray[marker++] = array[left++];
+                }
+            }
+
+            if (right <= end)
+            {
+                while (right <= end)
+                {
+                    tempArray[marker++] = array[right++];
+                }
+            }
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[start + i] = tempArray[i];
+            }
+        }
+
+        /// <summary>
+        /// Method which mutually exchanges the values of two variables.
+        /// </summary>
+        /// <param name="firstElement">First element to be swapped.</param>
+        /// <param name="secondElement">Second element to be swapped.</param>
+        private static void Swap(ref int firstElement, ref int secondElement)
+        {
+            int temp = firstElement;
+            firstElement = secondElement;
+            secondElement = temp;
+        }
+        #endregion
+    }
+}
